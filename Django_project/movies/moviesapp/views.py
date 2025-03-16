2b25e00e400d597fe5e0656528e1960d9157e3c7
val7978 <valerybutenko2020@gmail.com>
2025-03-16 20:20:13 +0500

    Add files via upload

---
+++ Django_project/movies/moviesapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+
+from . models import Movie
+
+class MoviesView(View):
+    """Список фильмов"""
+    def get (self, request):
+        movies = Movie.objects.all()
+        return render(request, "moviesapp/movie_list.html", {"movie_list": movies})
+    
